cb0fb51b5cb164fb7c12d0f9850a53c6dd27c4d1
Tejas-Nanaware <nanawaret@hotmail.com>
2018-10-24 14:59:47 +0530

    FeatureUpdate: Added data filtering

---
+++ App1Forms/DataFiltering.aspx
@@ -0,0 +1,20 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/MasterPage.master" AutoEventWireup="true" CodeFile="DataFiltering.aspx.cs" Inherits="DataFiltering" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="head" Runat="Server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" Runat="Server">
+	<div class="container">
+		<asp:Label ID="test" runat="server">Query</asp:Label>
+		<br />
+		<label>Search Name:</label>
+		<asp:TextBox id="NameSearch" runat="server"></asp:TextBox>
+		<label>Search Company:</label>
+		<asp:TextBox id="CompanySearch" runat="server"></asp:TextBox>
+		<asp:Button ID="NameSearchButton" runat="server" OnClick="Search" Text="Submit" CssClass="btn btn-primary" />
+		<br />
+		<br />
+		<asp:Label ID="NoResults" runat="server"></asp:Label>
+		<asp:GridView id="Users" runat="server"></asp:GridView>
+	</div>
+</asp:Content>
+
---
+++ App1Forms/DataFiltering.aspx.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class DataFiltering : System.Web.UI.Page
+{
+	protected void Page_Load(object sender, EventArgs e)
+	{
+		if(!Page.IsPostBack)
+		{
+			this.SearchUsers();
+		}
+	}
+	private void SearchUsers()
+	{
+		SqlConnection con = new SqlConnection();
+		con.ConnectionString = ConfigurationManager.ConnectionStrings["UserDetailsConnectionString"].ConnectionString;
+		con.Open();
+		SqlCommand cmd = new SqlCommand();
+		string query = "SELECT * FROM UserDetails";
+		if(!string.IsNullOrEmpty(NameSearch.Text.Trim()) || !string.IsNullOrEmpty(CompanySearch.Text.Trim()))
+		{
+			//query += " Where Name Like '%' @Name + '%'";
+			query += " WHERE Name LIKE '%"+NameSearch.Text.Trim()+"%' AND Company LIKE '%"+CompanySearch.Text.Trim()+"%'";
+			//cmd.Parameters.AddWithValue("@Name", NameSearch.Text.Trim());
+			test.Text = query;
+		}
+		cmd.CommandText = query;
+		cmd.Connection = con;
+		SqlDataAdapter sda = new SqlDataAdapter(cmd);
+		DataTable dt = new DataTable();
+		sda.Fill(dt);
+		Users.DataSource = dt;
+		Users.DataBind();
+		if (dt.Rows.Count == 0)
+		{
+			NoResults.Text = "No Results Found!";
+		}
+		else
+		{
+			Users.UseAccessibleHeader = true;
+			Users.HeaderRow.TableSection = TableRowSection.TableHeader;
+			Users.CssClass = "table table-striped table-sm";
+			Users.HeaderRow.CssClass = "thead-dark";
+		}
+	}
+	protected void Search(object Sender, EventArgs e)
+	{
+		NoResults.Text = "";
+		this.SearchUsers();
+	}
+	protected void OnPaging(object sender, GridViewPageEventArgs e)
+	{
+		Users.PageIndex = e.NewPageIndex;
+		this.SearchUsers();
+	}
+}
